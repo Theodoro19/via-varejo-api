67ce835d6b767b88daa165212d47f4bea0f9b3f0
Raphael Theodoro <raphaeltheodoro@hotmail.com>
2019-06-27 22:18:51 -0300

    fix produto

--- src/main/java/br/com/via/varejo/produto/model/Produto.java
+++ src/main/java/br/com/via/varejo/produto/model/Produto.java
@@ -1,20 +1,32 @@
 package br.com.via.varejo.produto.model;
 
+import static java.lang.Boolean.TRUE;
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.FetchType.LAZY;
+
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.hibernate.annotations.Where;
 
+import br.com.via.varejo.condicao.pagamento.model.CondicaoPagamento;
+import br.com.via.varejo.parcela.model.Parcela;
 import br.com.via.varejo.util.model.BaseEntity;
 
 /**
  * Classe onde será armazeda as informações sobre determinado produto
  *
  * @author rapha
- * @version 1.1
+ * @version 1.2
  *
  */
 @Entity
@@ -29,7 +41,17 @@ public class Produto extends BaseEntity {
 	private BigDecimal valor;
 
 	@Column(name = "prod_in_ativo")
-	private Boolean ativo = Boolean.TRUE;
+	private Boolean ativo = TRUE;
+
+	@OneToOne(mappedBy = "produto", fetch = LAZY, cascade = ALL)
+	@Fetch(FetchMode.SELECT)
+	@BatchSize(size = 100)
+	private CondicaoPagamento condicaoPagamento;
+
+	@OneToMany(mappedBy = "produto", fetch = LAZY, cascade = ALL)
+	@Fetch(FetchMode.SELECT)
+	@BatchSize(size = 100)
+	private List<Parcela> listaParcelas;
 
 	/**
 	 * @return the nome
@@ -71,6 +93,34 @@ public class Produto extends BaseEntity {
 	 */
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
+	}
+
+	/**
+	 * @return the condicaoPagamento
+	 */
+	public CondicaoPagamento getCondicaoPagamento() {
+		return condicaoPagamento;
+	}
+
+	/**
+	 * @param condicaoPagamento the condicaoPagamento to set
+	 */
+	public void setCondicaoPagamento(CondicaoPagamento condicaoPagamento) {
+		this.condicaoPagamento = condicaoPagamento;
+	}
+
+	/**
+	 * @return the listaParcelas
+	 */
+	public List<Parcela> getListaParcelas() {
+		return listaParcelas;
+	}
+
+	/**
+	 * @param listaParcelas the listaParcelas to set
+	 */
+	public void setListaParcelas(List<Parcela> listaParcelas) {
+		this.listaParcelas = listaParcelas;
 	}
 
 }
