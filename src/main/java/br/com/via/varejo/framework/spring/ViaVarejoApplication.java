e05394fcc2a7fdb9e6030d02182385b2d1b23892
Raphael Theodoro <raphaeltheodoro@hotmail.com>
2019-06-27 22:47:35 -0300

    Add application run

---
+++ src/main/java/br/com/via/varejo/framework/spring/ViaVarejoApplication.java
@@ -0,0 +1,14 @@
+package br.com.via.varejo.framework.spring;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaVarejoApplication {
+
+	public static void main(String[] args) {
+		
+		run(ViaVarejoApplication.class, args);
+	}
+}
