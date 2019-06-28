e3971a2257774d0136cf073fbf76b04e9e105a7c
Raphael Theodoro <raphaeltheodoro@hotmail.com>
2019-06-27 22:18:59 -0300

    [wip] controller

---
+++ src/main/java/br/com/via/varejo/produto/controller/ProdutoController.java
@@ -0,0 +1,15 @@
+package br.com.via.varejo.produto.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author rapha
+ * @version 1.0
+ */
+
+@RestController
+@RequestMapping(value = "produtos")
+public class ProdutoController {
+
+}
